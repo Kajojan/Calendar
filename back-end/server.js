3f05cf9b5b06e63858cee6a3ddd8aa371b5465a2
Kajojan <kojoty1999@gmail.com>
2022-12-20 12:47:37 +0100

    zmiana

--- back-end/server.js
+++ back-end/server.js
@@ -1,6 +1,7 @@
 require("dotenv").config();
 const express = require("express");
 const calRoutes=require('./routes/cal')
+const mongoose = require('mongoose')
 
 const app = express();
 
@@ -11,7 +12,14 @@ app.use((req, res, next) => {
 });
 
 app.use('/api/cal', calRoutes)
+mongoose.set("strictQuery", false);
 
-app.listen(process.env.PORT, () => {
-  console.log("listening on port 4000!");
-});
+mongoose.connect(process.env.MONGO_URI)
+    .then(()=>{
+        app.listen(process.env.PORT, () => {
+            console.log("listening on port",process.env.PORT );
+          });
+    })
+    .catch((error)=>{console.log(error)})
+
+
