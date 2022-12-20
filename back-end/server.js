c66120f8c94647e48e7fedda51a52ccea315a41d
Kajojan <kajetan.jankowski@interia.pl>
2022-12-20 12:33:55 +0100

    serwerRoutes

--- back-end/server.js
+++ back-end/server.js
@@ -1,13 +1,17 @@
-require('dotenv').config()
-const express = require('express')
+require("dotenv").config();
+const express = require("express");
+const calRoutes=require('./routes/cal')
 
-const app = express()
+const app = express();
 
-app.get('/', (req, res)=>{
-    res.json({mssg: 'Welcome to the app'})
-})
+app.use(express.json())
+app.use((req, res, next) => {
+  console.log(req.path, req.method);
+  next();
+});
 
+app.use('/api/cal', calRoutes)
 
-app.listen(process.env.PORT, ()=>{
-    console.log('listening on port 4000!')
-})
+app.listen(process.env.PORT, () => {
+  console.log("listening on port 4000!");
+});
