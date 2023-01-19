79bca0ae9a835dca2a86168f9ae92f5638ccaf52
Kajojan <kojoty1999@gmail.com>
2023-01-19 16:35:48 +0100

    zmiana

--- front-end/src/componets/mainCallendar/Day.js
+++ front-end/src/componets/mainCallendar/Day.js
@@ -1,12 +1,12 @@
-import React from "react";
+import React, { useState } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { useNavigate } from "react-router-dom";
-import { actions } from "../../features/CurrentDaySlice"
-
+import { actions } from "../../features/CurrentDaySlice";
 
 function Day() {
   const dispatch = useDispatch();
   const user = useSelector((state) => state.user.user_id);
+  const [days, setdays] = useState(32);
 
   const month = useSelector((state) => state.month.currentMonth);
   const currentYear = useSelector((state) => state.year.currentYear);
@@ -15,27 +15,34 @@ function Day() {
 
   const handleSubmit = (id) => {
     dispatch(actions.change(id));
-    dispatch(actions.changeData(cal.cal[month][id-1]))
+    dispatch(actions.changeData(cal.cal[month][id - 1]));
     navigate(`/callander/${user}/${currentYear}/${month}/${id}`);
+  };
+  const clickhandler = (a) => {
+    setdays(a)
   };
 
   return (
     <div>
-      {cal.cal[month].map((el) => {
-            const key = `${el.month_Id}_${el.id}`;
-            return (
-              <button key={key} onClick={() => handleSubmit(el.id)}>
-                {el.id}
-                {
-                  <ul>
-                    {el.event.map((ele, index) => {
-                      return ele == null ? null : <a key={index}>{ele.name}</a>;
-                    })}
-                  </ul>
-                }
-              </button>
-            );
-        
+      <button onClick={() => clickhandler(7)}>Week view</button>
+      <button onClick={() => clickhandler(32)}>Month view</button>
+
+      {cal.cal[month].map((el,index) => {
+        if(index < days){
+        const key = `${el.month_Id}_${el.id}`;
+        return (
+          <button key={key} onClick={() => handleSubmit(el.id)}>
+            {el.id}
+            {
+              <ul>
+                {el.event.map((ele, index) => {
+                  return ele == null ? null : <a key={index}>{ele.name}</a>;
+                })}
+              </ul>
+            }
+          </button>
+        );
+          }
       })}
     </div>
   );
