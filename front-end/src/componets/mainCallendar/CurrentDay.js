354abc1245a393812b3edcc8532ce021874f1f6f
Kajojan <kojoty1999@gmail.com>
2023-01-19 16:11:44 +0100

    zmiana

--- front-end/src/componets/mainCallendar/CurrentDay.js
+++ front-end/src/componets/mainCallendar/CurrentDay.js
@@ -35,8 +35,9 @@ function CurrentDay() {
       ) : (
         <a>None Of Event</a>
       )}{console.log(pop)}
+      {cal.users ? <>
       {pop[0] && [cal.users.admin[0] ==user_id || cal.users.reader[0] == user_id ]  ? <PopUp setPop={setPop} pop={pop}/> : null }
-      {cal.users.admin[0] ==user_id || cal.users.reader[0] == user_id ?  <EventForm name={"Add Event"} pop={pop} /> : null }
+      {cal.users.admin[0] ==user_id || cal.users.reader[0] == user_id ?  <EventForm name={"Add Event"} pop={pop} /> : null }</> : null}
     </div>
   );
 }
