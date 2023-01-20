b91f3852ba4c539bf36c9c52278c8d0902677f15
Kajojan <kojoty1999@gmail.com>
2023-01-20 11:16:39 +0100

    zmiana

--- front-end/src/componets/mainCallendar/Day.js
+++ front-end/src/componets/mainCallendar/Day.js
@@ -12,6 +12,9 @@ function Day() {
   const currentYear = useSelector((state) => state.year.currentYear);
   const cal = useSelector((state) => state.cal.cal);
   const navigate = useNavigate();
+  const [search, setSearch] = useState('')
+  const [data, setData]=useState([])
+  const [click, setClick]=useState(false)
 
   const handleSubmit = (id) => {
     dispatch(actions.change(id));
@@ -21,9 +24,52 @@ function Day() {
   const clickhandler = (a) => {
     setdays(a)
   };
+  const searchhandler=(event)=>{
+    setClick(false)
+    setSearch(event.target.value)
 
+  }
+  const monthNames = [
+    "Styczeń",
+    "Luty",
+    "Marzec",
+    "Kwiecie",
+    "Maj",
+    "Czerwiec",
+    "Lipiec",
+    "Sierpień",
+    "Wrzesień",
+    "Październik",
+    "Listopad",
+    "Grudzień",
+  ];
+  const Clicksearch=()=>{
+    const events=[]
+    cal.cal.map((ele,index)=>{
+       ele.map((ele2,index2)=>{
+          if(ele2.event.length > 0 ){
+              ele2.event.forEach(element=>{
+                if(element.name.toLowerCase() == search.toLowerCase() ){ events.push([element, ele2]) }
+              })
+          }
+      })
+    })
+    setData(events)
+    setClick(true)
+
+  }
+ 
   return (
     <div>
+      <div className="search">
+      <input type="search" placeholder="Search event" value={search} onChange={searchhandler}></input>
+      <button onClick={Clicksearch}>Search</button>
+      <div className="searchEvents">
+        {data.length > 0 ? (data.map((ele,index)=>{
+          return <a key={index}>{ele[0].name}-{ele[0].time ? "AllDay" : ele[0].start - ele[0].end}, date: {monthNames[ele[1].month_Id]}, {ele[1].id} </a>
+        })): click ? <a>Not found "{search}"</a>: null}
+        </div>
+      </div>
       <button onClick={() => clickhandler(7)}>Week view</button>
       <button onClick={() => clickhandler(32)}>Month view</button>
 
@@ -49,3 +95,5 @@ function Day() {
 }
 
 export default Day;
+
+
